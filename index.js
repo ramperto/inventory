92c6a0617773c1370d78593eac6cfcf65a77a404
ramperto <rampertopasaribu@gmail.com>
2020-10-02 16:07:40 +0000

    add mongodb localhost

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
 // launch our server and connect to database
 const PORT = process.env.PORT || 5000;
 //const mongodb_url = process.env.MONGODB_URL;
-const mongodb_url = mongodb://127.0.0.1/inventory;
+const mongodb_url = 'mongodb://localhost:27017/inventory';
 mongoose
 	.connect(mongodb_url, {
 		useNewUrlParser: true,
