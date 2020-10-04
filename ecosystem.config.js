99a094321d6d5f702af7d0054c6db1b8b639bff5
ramperto <rampertopasaribu@gmail.com>
2020-10-04 23:30:16 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         "name"            : "prod-inventory",
         "script"          : "/home/ramperto/inventory/index.js",
         "instances"       : "max",
-        "exec_mode"       : "cluster" 
+        "exec_mode"       : "cluster", 
         "log_date_format" : "YYYY-MM-DD HH:mm Z",
         "env"  : {
             "PORT": 5000
