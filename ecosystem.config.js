bcefecf30f117f74a80e13992bc986d4b0eeaf21
ramperto <ramperto@member.id>
2021-01-28 08:21:24 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "apps" : [
       {
         "name"            : "prod-inventory",
-        "script"          : "/home/ramperto/inventory/index.js",
+        "script"          : "../index.js",
         "instances"       : "max",
         "exec_mode"       : "cluster", 
         "log_date_format" : "YYYY-MM-DD HH:mm Z",
